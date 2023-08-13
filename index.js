3fa7fda1ebb618148df0ccc61f6efffb5e420609
Khemmie-Ray <atokemmy@gmail.com>
2023-08-13 21:28:05 +0100

    fixing css bugs

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const signUpButton = document.getElementById("signUp");
-const signInButton = document.getElementById("signIp");
+const signUpButton = document.getElementById("signup");
+// const signInButton = document.getElementById("signIp");
 const container = document.getElementById("container");
 
 signUpButton.addEventListener("click", () => {
   container.classList.add("right-panel-active");
 });
 
-signInButton.addEventListener("click", () => {
-  container.classList.remove("right-panel-active");
-});
+// signInButton.addEventListener("click", () => {
+//   container.classList.remove("right-panel-active");
+// });
